091e62fe38323a6f739b52ab4c90c32dd52add1a
THE-HYC <862807018@qq.com>
2020-10-31 20:21:50 +0800

    提交

---
+++ src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    private int id;
+    private int name;
+
+    public static void main(String[] args) {
+
+        int i=0;
+        int j=1;
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+}
