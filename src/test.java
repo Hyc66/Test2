75a6c029baa2680827547193b8a44b3f11453d99
THE_Word <862807018@qq.com>
2020-10-31 20:52:12 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -5,10 +5,10 @@ public class test {
     public static void main(String[] args) {
 
         int i=0;
-        int j=1;
+        int a=1;
         int d=2;
         System.out.println(i);
-        System.out.println(j);
+        System.out.println(a);
         System.out.println(d);
     }
 
